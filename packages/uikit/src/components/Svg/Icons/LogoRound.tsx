2d54e492db10fa2460cba2f44c152230e5e85036
SavefyLabs <tukarxyz@gmail.com>
2025-03-03 14:35:10 +0700

    first commit

---
+++ packages/uikit/src/components/Svg/Icons/LogoRound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Svg from "../Svg";
+import { SvgProps } from "../types";
+
+const Icon: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
+  return (
+    <Svg viewBox="0 0 96 96" {...props}>
+    </Svg>
+  );
+};
+
+export default Icon;
